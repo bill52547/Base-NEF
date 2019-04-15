616b91e563b25c02db237483b02f51c6dc3b05c0
Minghao <mh.guo0111@gmail.com>
2019-04-15 09:43:17 +0800

    hang

--- nef/version.py
+++ nef/version.py
@@ -2,7 +2,7 @@
 # THIS FILE IS GENERATED FROM NEF SETUP.PY
 # 
 short_version = '0.0.0.1'
-full_version = '0.0.0.1.1555228217'
-generated_time = '2019-04-14 15:50:17'
+full_version = '0.0.0.1.1555263140'
+generated_time = '2019-04-15 01:32:20'
     
     
